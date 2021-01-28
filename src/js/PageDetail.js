b71aade51ceae41f096eaa3823ab00ed7a3f3088
Mathieu JOLY <m.joly@live.fr>
2021-01-28 12:18:14 +0100

    add button attributes layout

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -9,9 +9,11 @@ const PageDetail = (argument) => {
       fetch(`${finalURL}`)
         .then((response) => response.json())
         .then((response) => {
-          let { name, released, description, background_image } = response;
+          console.log(response);
+          let { name, released, description, background_image, website } = response;
           let articleDOM = document.querySelector(".pageDetail .pageDetail__article");
           articleDOM.querySelector(".pageDetail__article__banner img").src = background_image;
+          articleDOM.querySelector(".pageDetail__article__banner button").url = website;
           articleDOM.querySelector("h1.pageDetail__article__title").innerHTML = name;
           articleDOM.querySelector("p.pageDetail__article__releaseDate span").innerHTML = released;
           articleDOM.querySelector("p.pageDetail__article__description").innerHTML = description;
@@ -29,6 +31,7 @@ const PageDetail = (argument) => {
         <div class="pageDetail__article">
           <div class="pageDetail__article__banner">
             <img class='pageDetail__article__banner__image' src="">
+            <button class='pageDetail__article__banner__button' url="">Check website</button>
           </div>
           <h1 class="pageDetail__article__title"></h1>
           <p class="pageDetail__article__releaseDate">Release date : <span></span></p>
