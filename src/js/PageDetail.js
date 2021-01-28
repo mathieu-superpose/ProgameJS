4b821ab83300dae1cf599d84e0ac7842e73e5108
Mathieu JOLY <m.joly@live.fr>
2021-01-28 13:30:31 +0100

    add banner div rename classes

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -11,9 +11,11 @@ const PageDetail = (argument) => {
         .then((response) => {
           console.log(response);
           let { name, released, description, background_image, website, rating, ratings_count } = response;
+          let bannerDOM = document.querySelector(".pageDetail .pageDetail__banner");
+          bannerDOM.querySelector(".pageDetail__banner img").src = background_image;
+          bannerDOM.querySelector(".pageDetail__banner__form").action = website;
+
           let articleDOM = document.querySelector(".pageDetail .pageDetail__article");
-          articleDOM.querySelector(".pageDetail__article__banner img").src = background_image;
-          articleDOM.querySelector(".pageDetail__article__banner__form").action = website;
           articleDOM.querySelector("h1.pageDetail__article__head__title").innerHTML = name;
           articleDOM.querySelector("p.pageDetail__article__head__rating").innerHTML = `${rating}/5 - ${ratings_count} votes`;
           articleDOM.querySelector("p.pageDetail__article__releaseDate span").innerHTML = released;
@@ -29,13 +31,13 @@ const PageDetail = (argument) => {
   const render = () => {
     pageContent.innerHTML = `
       <section class="pageDetail">
+        <div class="pageDetail__banner">
+          <img class='pageDetail__banner__image' src="">
+          <form class="pageDetail__banner__form" action="">
+            <input class='pageDetail__banner__button' type="submit" value="Check website" />
+          </form>
+        </div>
         <div class="pageDetail__article">
-          <div class="pageDetail__article__banner">
-            <img class='pageDetail__article__banner__image' src="">
-            <form class="pageDetail__article__banner__form" action="">
-              <input class='pageDetail__article__banner__button' type="submit" value="Check website" />
-            </form>
-          </div>
           <div class="pageDetail__article__head">
             <h1 class="pageDetail__article__head__title"></h1>
             <p class="pageDetail__article__head__rating"></p>
