8275a77c69e9d92000ee0462becb100adf538f0e
Mathieu JOLY <m.joly@live.fr>
2021-01-28 17:38:07 +0100

    add buy

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -6,46 +6,55 @@ const PageDetail = (argument) => {
       let finalURL = url + argument;
 
       const getRating = (rating, ratings_count) => {
-            return `${rating}/5 - ${ratings_count} votes`;
+        return `${rating}/5 - ${ratings_count} votes`;
       };
 
       const getPlatforms = (platforms) => {
-            return platforms.map(plat=> plat.platform.name).join('<br>');
+        return platforms.map(plat=> plat.platform.name).join('<br>');
       };
 
       const getGenres = (genres) => {
-            return genres.map(genre=> genre.name).join('<br>');
+        return genres.map(genre=> genre.name).join('<br>');
       };
 
       const getTags = (tags) => {
-            return tags.map(tag=> tag.name).reduce((r, e, i) =>
+        return tags.map(tag=> tag.name).reduce((r, e, i) =>
     (i % 4 ? r[r.length - 1].push(e) : r.push([e])) && r, []).join('<br>').replace(/,/g, ',  ');
       };
+
+      const getStores = (stores) => {
+        return stores.map(str=>`<a href="${str.url}">${str.store.name}</a>`).join('<br>');
+      }
 
       fetch(`${finalURL}`)
         .then((response) => response.json())
         .then((response) => {
           console.log(response);
-          let { name, released, description, background_image, website, rating, ratings_count, developers, platforms, publishers, genres, tags } = response;
+          let { name, released, description, background_image, website, rating, ratings_count, developers, platforms, publishers, genres, tags, stores } = response;
 
-          let bannerDOM = document.querySelector(".pageDetail .pageDetail__banner");
+          let pageDetailDOM = document.querySelector(".pageDetail");
+
+          let bannerDOM = pageDetailDOM.querySelector(".pageDetail__banner");
           bannerDOM.querySelector(".pageDetail__banner img").src = background_image;
           bannerDOM.querySelector(".pageDetail__banner__form").action = website;
 
-          let articleDOM = document.querySelector(".pageDetail .pageDetail__article");
+          let articleDOM = pageDetailDOM.querySelector(".pageDetail__article");
           articleDOM.querySelector("h1.pageDetail__article__head__title").innerHTML = name;
           articleDOM.querySelector("h3.pageDetail__article__head__rating").innerHTML = getRating(rating, ratings_count);
           articleDOM.querySelector("p.pageDetail__article__description").innerHTML = description;
 
-          let details1DOM = document.querySelector(".pageDetail .pageDetail__article .pageDetail__article__details1");
+          let details1DOM = articleDOM.querySelector(".pageDetail__article__details1");
           details1DOM.querySelector("p.pageDetail__article__details1__releaseDate span").innerHTML = released;
           details1DOM.querySelector("p.pageDetail__article__details1__developer span").innerHTML = developers[0].name;
           details1DOM.querySelector("p.pageDetail__article__details1__platforms span").innerHTML = getPlatforms(platforms);
           details1DOM.querySelector("p.pageDetail__article__details1__publishers span").innerHTML = publishers[0].name;
           
-          let details2DOM = document.querySelector(".pageDetail .pageDetail__article .pageDetail__article__details2");
+          let details2DOM = articleDOM.querySelector(".pageDetail__article__details2");
           details2DOM.querySelector("p.pageDetail__article__details2__genres span").innerHTML = getGenres(genres);
           details2DOM.querySelector("p.pageDetail__article__details2__tags span").innerHTML = getTags(tags);
+
+          let buyDOM = articleDOM.querySelector(".pageDetail__article__buy");
+          buyDOM.querySelector("p.pageDetail__article__buy__stores span").innerHTML = getStores(stores);
 
           if (name.length>20) articleDOM.querySelector("h1.pageDetail__article__head__title").style.fontSize = "24px";
         });
@@ -81,7 +90,11 @@ const PageDetail = (argument) => {
           <div class="pageDetail__article__details2">
             <p class="pageDetail__article__details2__genres"><b>Genres</b><br><span></span></p>
             <p class="pageDetail__article__details2__tags"><b>Tags</b><br><span></span></p>
-          </div>       
+          </div>
+          <div class="pageDetail__article__buy">
+            <h1 class="pageDetail__article__buy__title">BUY</h1>
+            <p class="pageDetail__article__buy__stores"><span></span></p>
+          </div>              
         </div>
       </section>
     `;
