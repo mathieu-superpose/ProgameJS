067bc768b8a8adc217c5fee88a76d29495b39305
Mathieu JOLY <m.joly@live.fr>
2021-01-28 11:09:25 +0100

    add banner layout

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -10,11 +10,11 @@ const PageDetail = (argument) => {
         .then((response) => response.json())
         .then((response) => {
           let { name, released, description, background_image } = response;
-          let articleDOM = document.querySelector(".page-detail .article");
-          articleDOM.querySelector(".banner img").src = background_image;
-          articleDOM.querySelector("h1.title").innerHTML = name;
-          articleDOM.querySelector("p.release-date span").innerHTML = released;
-          articleDOM.querySelector("p.description").innerHTML = description;
+          let articleDOM = document.querySelector(".pageDetail .pageDetail__article");
+          articleDOM.querySelector(".pageDetail__article__banner img").src = background_image;
+          articleDOM.querySelector("h1.pageDetail__article__title").innerHTML = name;
+          articleDOM.querySelector("p.pageDetail__article__releaseDate span").innerHTML = released;
+          articleDOM.querySelector("p.pageDetail__article__description").innerHTML = description;
         });
     };
 
@@ -23,14 +23,14 @@ const PageDetail = (argument) => {
 
   const render = () => {
     pageContent.innerHTML = `
-      <section class="page-detail">
-        <div class="article">
-          <div class="banner">
-            <img class='page-list__articles__cardGame__cardImage-image' src="">
+      <section class="pageDetail">
+        <div class="pageDetail__article">
+          <div class="pageDetail__article__banner">
+            <img class='pageDetail__article__banner__image' src="">
           </div>
-          <h1 class="title"></h1>
-          <p class="release-date">Release date : <span></span></p>
-          <p class="description"></p>
+          <h1 class="pageDetail__article__title"></h1>
+          <p class="pageDetail__article__releaseDate">Release date : <span></span></p>
+          <p class="pageDetail__article__description"></p>
         </div>
       </section>
     `;
